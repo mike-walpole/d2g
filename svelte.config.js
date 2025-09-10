bf6a6567ea6fb3ec7ac17ded2809a8fd1e51c397
mike-walpole <walpole.mike@gmail.com>
2025-09-10 12:34:40 +0200

    Update SvelteKit configuration for Vercel deployment
    - Modified the SvelteKit configuration to specify the Node.js runtime version as 'nodejs20.x' and set the deployment region to 'iad1'.
    - This change ensures compatibility with the latest Vercel features and optimizes the deployment settings for improved performance.
    
    This commit enhances the application's deployment configuration for better integration with Vercel.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs20.x',
+			regions: ['iad1']
+		})
+	}
+};
 
 export default config;
